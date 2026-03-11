6ac00f95b7b39c05aca43b968607b4221b088d78
hatttoomma <1053003573@qq.com>
2026-03-11 20:17:43 +0800

    1

--- verl_grpo/run_qwen3vl_grpo.sh
+++ verl_grpo/run_qwen3vl_grpo.sh
@@ -37,11 +37,11 @@ python3 -m verl.trainer.main_ppo \
   actor_rollout_ref.rollout.n=1 \
   actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=1 \
   actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=1 \
-  actor_rollout_ref.rollout.engine_kwargs.vllm.kv_cache_dtype=fp8 \
-  actor_rollout_ref.actor.fsdp_config.param_offload=True \
-  actor_rollout_ref.actor.fsdp_config.grad_offload=True \
-  actor_rollout_ref.actor.fsdp_config.optimizer_offload=True \
-  actor_rollout_ref.model.enable_activation_offload=True \
+  +actor_rollout_ref.rollout.engine_kwargs.vllm.kv_cache_dtype=fp8 \
+  +actor_rollout_ref.actor.fsdp_config.param_offload=True \
+  +actor_rollout_ref.actor.fsdp_config.grad_offload=True \
+  +actor_rollout_ref.actor.fsdp_config.optimizer_offload=True \
+  +actor_rollout_ref.model.enable_activation_offload=True \
   custom_reward_function.path="$(pwd)/verl_grpo/reward_vdr.py" \
   custom_reward_function.name=compute_score \
   algorithm.use_kl_in_reward=False \
